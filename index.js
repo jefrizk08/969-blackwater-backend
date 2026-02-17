a12dfbe580d0c4fd3ef94c68cf81970f739d1ee3
Jefri <jefrizk72423@gmail.com>
2026-02-18 06:58:45 +0700

    first backend 969 blackwater

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        name: "969 BLACK WATER CLUB",
+        status: "Backend aktif 🚀"
+    })
+})
+
+app.get("/donations", (req, res) => {
+    res.json([
+        { username: "Jefri", amount: 969 },
+        { username: "BlackWater", amount: 1500 }
+    ])
+})
+
+app.listen(3000, () => {
+    console.log("Backend jalan di http://localhost:3000")
+})
